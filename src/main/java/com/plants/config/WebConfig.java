e445d53ef900dd90b21ec47da64b579af1ae6674
Plant ropan <plantropan24@gmail.com>
2024-11-23 20:57:43 -0500

    web config class new

---
+++ src/main/java/com/plants/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.plants.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	            .allowedOrigins("http://localhost:8080", "https://planrropanbackend-production.up.railway.app/")  // Add both local and deployed URLs
+	            .allowedMethods("GET", "POST", "PUT", "DELETE");
+	}
+}
